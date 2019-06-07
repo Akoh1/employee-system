713fd932fb815c7d07f793f73219b72788e95e79
akoh1 <samuela@ehealth4everyone.com>
2019-06-07 15:44:26 +0100

    change default to pgsql

--- config/database.php
+++ config/database.php
@@ -14,7 +14,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    // 'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
