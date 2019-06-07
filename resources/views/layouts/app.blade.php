1225c8bb3b01e109be5628adbe286337a0cb55d8
akoh1 <samuela@ehealth4everyone.com>
2019-06-07 22:32:00 +0100

    clean up header auth test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,7 +54,7 @@
                           <li class="nav-item dropdown">
 
                               <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                  {{ Auth::guard('regularuser')->user()->name }} <span class="caret"></span>
+                                  {{ Auth::guard('regularuser')->name() }} <span class="caret"></span>
                               </a>
 
 
